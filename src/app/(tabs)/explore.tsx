fe7c592628b6c89e2602031ece933f28ce62348c
mahmud0202 <bakytbekovich055@gmail.com>
2026-02-19 09:58:55 +0600

    props

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,5 +1,41 @@
-import { View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function TabTwoScreen() {
-  return <View style={{ flex: 1, backgroundColor: "#fff" }}></View>;
+  const navigation = useNavigation<any>();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Вторая вкладка</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("my")}
+      >
+        <Text style={styles.buttonText}>Перейти </Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f4f6f8",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 22,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#75868b",
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
