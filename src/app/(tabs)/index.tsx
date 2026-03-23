ec915966ab98a67b528382a059a03da7186c9b2b
ulugbek <makkughonov_ua@bhu.kg>
2026-03-23 10:51:14 +0600

    api promise

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import CounterButton from "@/components/CounterButton";
 import {
   DancingScript_400Regular,
   useFonts,
@@ -168,3 +169,18 @@ const styles = StyleSheet.create({
     fontFamily: "DancingScript_400Regular",
   },
 });
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      {/* <ProfileCard name="asd" course="DEV" role="Student" btnName="Press me" btnStep={1056} */}
+
+      <CounterButton title="Click Me1" step={1} initialValue={0} />
+      <CounterButton title="Click Me2" step={2} initialValue={0} />
+      <CounterButton title="Click Me3" step={3} initialValue={0} />
+      <CounterButton title="Click Me4" step={4} initialValue={0} />
+
+      {/* <Greeting name="asd" role="teacher"/> */}
+    </View>
+  );
+}
